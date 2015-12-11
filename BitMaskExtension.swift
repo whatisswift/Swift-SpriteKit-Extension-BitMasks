3fc0c44e6ae8e1e0a3bf15c81a46b2f606294cde
whatisswift <thisismyrealmailaddressreally@gmail.com>
2015-12-11 20:34:53 +0900

    Update BitMaskExtension.swift

--- BitMaskExtension.swift
+++ BitMaskExtension.swift
@@ -1,1 +1,91 @@
+//
+//  BitMaskExtension.swift
+//
+//  Created by Daichi Nagahamaya on 12/11/15.
+//  Copyright © 2015 BigPen. All rights reserved.
+//
+
+import SpriteKit
+
+extension SKSpriteNode{
+    override func setBitMasks(categoryBitMask categoryBitMask: UInt32, collisionBitMask: UInt32, contactTestBitMask: UInt32){
+        super.setBitMasks(categoryBitMask: categoryBitMask, collisionBitMask: collisionBitMask, contactTestBitMask: contactTestBitMask)
+    }
+    
+    
+    override func setBitMasks(categoryBitMask categoryBitMask: UInt32, contactTestBitMask: UInt32){
+        super.setBitMasks(categoryBitMask: categoryBitMask, collisionBitMask: contactTestBitMask)
+    }
+    
+    
+    override func setBitMasks(categoryBitMask categoryBitMask: UInt32, collisionBitMask: UInt32){
+        super.setBitMasks(categoryBitMask: categoryBitMask, collisionBitMask: collisionBitMask)
+    }
+    
+    
+    override func setBitMasks(collisionBitMask: UInt32, contactTestBitMask: UInt32){
+        super.setBitMasks(collisionBitMask, contactTestBitMask: contactTestBitMask)
+    }
+    
+    
+    override func setBitMask(contactTestBitMask contactTestBitMask: UInt32){
+        super.setBitMask(contactTestBitMask: contactTestBitMask)
+    }
+    
+    
+    override func setBitMask(collisionBitMask collisionBitMask: UInt32){
+        super.setBitMask(collisionBitMask: collisionBitMask)
+    }
+    
+    
+    override func setBitMask(categoryBitMask categoryBitMask: UInt32){
+        super.setBitMask(categoryBitMask: categoryBitMask)
+        
+    }
+}
+
+
+extension SKNode{
+    func setBitMasks(categoryBitMask categoryBitMask: UInt32, collisionBitMask: UInt32, contactTestBitMask: UInt32){
+        self.physicsBody!.categoryBitMask = categoryBitMask
+        self.physicsBody!.collisionBitMask = collisionBitMask
+        self.physicsBody!.contactTestBitMask = contactTestBitMask
+    }
+    
+    
+    func setBitMasks(categoryBitMask categoryBitMask: UInt32, contactTestBitMask: UInt32){
+        self.physicsBody!.categoryBitMask = categoryBitMask
+        self.physicsBody!.contactTestBitMask = contactTestBitMask
+    }
+    
+    
+    func setBitMasks(categoryBitMask categoryBitMask: UInt32, collisionBitMask: UInt32){
+        self.physicsBody!.categoryBitMask = categoryBitMask
+        self.physicsBody!.collisionBitMask = collisionBitMask
+    }
+    
+    
+    func setBitMasks(collisionBitMask: UInt32, contactTestBitMask: UInt32){
+        self.physicsBody!.collisionBitMask = collisionBitMask
+        self.physicsBody!.contactTestBitMask = contactTestBitMask
+    }
+    
+    
+    func setBitMask(contactTestBitMask contactTestBitMask: UInt32){
+        self.physicsBody!.contactTestBitMask = contactTestBitMask
+    }
+    
+    
+    func setBitMask(collisionBitMask collisionBitMask: UInt32){
+        self.physicsBody!.collisionBitMask = collisionBitMask
+    }
+    
+    
+    func setBitMask(categoryBitMask categoryBitMask: UInt32){
+        self.physicsBody!.categoryBitMask = categoryBitMask
+        
+    }
+}
+
+
 
